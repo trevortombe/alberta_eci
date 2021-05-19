92a20adcfca5db90c1db12b62e824df5ad04b664
Trevor Tombe <trevor.tombe@gmail.com>
2021-05-19 13:28:50 -0600

    Create github action to apply dates to datawrapper plots

---
+++ UpdateDatawrapper.R
@@ -0,0 +1,31 @@
+
+packages<-c("curl","scales","zoo","DatawRappr")
+check.packages <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+# Authorize API Access to Datawrapper
+datawrapper_auth(api_key = Sys.getenv("DATAWRAPPERAUTH"))
+
+# The plot ID of the datawrapper
+eci<-"fIIKi"
+
+# Fetch the ECI Data
+latestECI<-read.csv('https://github.com/trevortombe/alberta_eci/raw/master/ECI_Index_Data.csv')
+end_x_year<-as.character(number(as.numeric(as.yearmon(as.character(latestECI[dim(latestECI)[1],1]),"%b %Y")+2.5),.1,big.mark = ""))
+dw_edit_chart(eci,annotate="Test")
+eci_data<-dw_retrieve_chart_metadata(eci)
+
+eci_data[[1]]$metadata$visualize$`text-annotations`[[1]]$y
+visual_meta<-eci_data[[1]]$metadata$visualize
+visual_meta$`text-annotations`[[1]]$y<-number(latestECI[dim(latestECI)[1],2],.01) # update point to latest
+visual_meta$`text-annotations`[[1]]$x<-as.character(number(as.numeric(as.yearmon(as.character(latestECI[dim(latestECI)[1],1]),"%b %Y")),.1,big.mark = "")) # update point to latest
+visual_meta$`text-annotations`[[1]]$text<-paste("Latest:",number(latestECI[dim(latestECI)[1],2],.01)) # update point to index value
+visual_meta$`custom-range-x`[[2]]
+dw_edit_chart(eci,visualize = visual_meta)
+
+dw_edit_chart(eci,title=paste("Alberta Economic Conditions Index to",as.character(latestECI[dim(latestECI)[1],1])))
+
